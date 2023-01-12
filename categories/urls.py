6c00bfde772537df68bb0a174da54141ab35b4c3
Sebastian <sebastian.nmb5@gmail.com>
2023-01-12 13:59:18 -0300

    Forms in categories

--- categories/urls.py
+++ categories/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from categories.views import create_category, list_categories
 
 urlpatterns = [
-    path('create-category/<str:name>/', create_category),
+    path('create-category/', create_category),
     path('list-categories/', list_categories),
 
 ]
--- categories/views.py
+++ categories/views.py
@@ -2,12 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from categories.models import Category
+from categories.forms import CategoryForm
 
 # Create your views here.
 
-def create_category(request, name):
-    Category.objects.create(name=name)
-    return HttpResponse('Categoría creada')
+def create_category(request):
+    if request.method == 'GET':
+        context = {
+            'form': CategoryForm()
+        }
+        return render(request, 'categories/create_category.html', context={})
+
+    elif request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            #Creamos la categoria
+            Category.objects.create(
+                name=form.cleaned_data['name'],
+            )
+            context = {
+                'message': 'Categoría creada exitosamente'
+            }
+            return render(request, 'categories/create_category.html', context=context)
+        
+        else: 
+            context = {
+                'form_errors': form.errors,
+            }
+            
+        return render(request, 'categories/create_category.html', context={})
 
 def list_categories(request):
     all_categories = Category.objects.all()
@@ -15,5 +38,4 @@ def list_categories(request):
         'categories':all_categories
     }
     return render(request, 'categories/list_categories.html', context=context)
-
 
